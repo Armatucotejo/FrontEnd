ee52474ae806f2da5be9781d9ee5855689989f08
kevin vanegas <kavanegasm@unal.edu.co>
2018-10-21 17:38:16 -0500

    add react turbolinks webpaked
    https://zayne.io/blog/how-to-create-a-react-app-with-ruby-on-rails-part-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get "welcome/index"
-  get "register" => "welcome#register"
-  resources :users
-  #=> "welcome#register"
-  post "welcome/register"
-  root "welcome#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'welcome#index'
 end
