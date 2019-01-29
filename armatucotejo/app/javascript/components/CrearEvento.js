fea6ddadbce3f83d97e4100b3f6deeb56518d340
gimg1859 <gimg1859@github.com>
2019-01-28 19:12:58 -0500

    Por favor ingrese el mensaje del commit para sus cambios. Las líneas que comiencen  con '#' serán ignoradas, y un mensaje vacío aborta el commit.
     En la rama iteracion5vistas
     Tu rama está actualizada con 'origin/iteracion5vistas'.
    
     Cambios a ser confirmados:
    	modificado:     app/assets/stylesheets/style.css
    	nuevo archivo:  app/javascript/Captura de pantalla de 2019-01-28 19-06-40.png
    	modificado:     app/javascript/components/CrearEvento.js

--- armatucotejo/app/javascript/components/CrearEvento.js
+++ armatucotejo/app/javascript/components/CrearEvento.js
@@ -19,7 +19,7 @@ class CrearEvento extends React.Component{
               <div id="map"></div>
               </div>
               <div className="fivebc col-3">
-            <h2>Crear Evento</h2>
+            <h2 class="formulario">Crear Evento</h2>
               <div className="col">
               <select id="inputState" class="form-control">
                 <option selected>Deportes</option>
@@ -31,19 +31,41 @@ class CrearEvento extends React.Component{
               </div>
               <h6> </h6>
               <div class="formulario">
-                <form name="formulario">
+                <div class="formulario">
+                  Lugar
+                  <input type="text" name="" value="Lugar"></input>
+                </div>
+                <form name="formulario2">
+                  <div class="formulario2">
+                    Selecciona la fecha deseada:
+                    <input type="date" name="fecha" min="2019-03-25"
+                                                    max="2019-05-25" step="2"></input>
+                  </div>
+                  <div class="formulario2">
+                    Selecciona la hora deseada:
+                    <input type="time" name="hora" min="18:00"
+                    max="21:00" step="3600"></input>
+                    <h6> </h6>
+                  </div>
 
-                  Selecciona la fecha deseada:
-                  <input type="date" name="fecha" min="2019-03-25"
-                                                  max="2019-05-25" step="2"></input>
-
-                  Selecciona la hora deseada:
-                  <input type="time" name="hora" min="18:00"
-                                                 max="21:00" step="3600"></input>
                 </form>
-
+                <div class="formulario2">
+                  <div className="col">
+                  <select id="inputState" class="form-control">
+                    <option selected>Jugadores</option>
+                    <option>Jugador1</option>
+                    <option>Jugador2</option>
+                    <option>Jugador3</option>
+                    <option>Jugador4</option>
+                  </select>
+                  <h6> </h6>
+                  </div>
+                </div>
+                <div class="formulario">
+                  <input type="text" name="" value="Description"></input>
+                </div>
               </div>
-
+              <div class="center divbutton"><a class="link nav-link" href="/crearevento"><button class="btn buttonhome btn-lg thirdbc ">Crear</button></a><h6 class="comfortaa firstc regbutton"></h6></div>
               </div>
             </div>
           </div>
