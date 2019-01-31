808d5f195277bf341f5f6ce64887772406bf34bf
Unknown <ggalvezb@unal.edu.co>
2019-01-31 13:47:46 -0500

    Added view Profile Beta
    4 files modified, added pngs and jss.

---
+++ armatucotejo/app/javascript/components/Perfill.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logonav from '../../assets/images/logo-nav.png';
+import Navgbar from './Navgbar';
+class Perfil extends React.Component{
+
+	
+
+
+
+	render(){
+		return(
+			<Navgbar/>
+			)
+	}
+}
+
+
+export default Perfil
